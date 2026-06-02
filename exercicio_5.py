e9b13e58edd8bc08bb7efcf4d56edf43be1c2561
annietakaesu-coder <annie.takaesu@gmail.com>
2026-06-02 07:46:17 -0300

    exercicio_5 - resolvido
    Add logic to find the organism with the highest average.

--- exercicio_5.py
+++ exercicio_5.py
@@ -40,3 +40,17 @@ O cálculo de média já foi feito em sala e pode ser usado de exemplo.
 lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
 # Fazer a partir daqui
+
+maior_indice = 0
+maior_media = 0
+
+for lista in range(len(lista_de_organismos)):
+    soma = 0
+    for valor in lista_de_organismos[lista]:
+        soma = soma + valor
+    media = soma / len(lista_de_organismos[lista])
+    if media > maior_media:
+        maior_media = media
+        maior_indice = lista
+
+print(f'O organismo com maior média é o da posição {maior_indice} da lista.')
