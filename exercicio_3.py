8c17570cf2c8bba752b3ca3ad2a59be33efef95e
annietakaesu-coder <annie.takaesu@gmail.com>
2026-06-02 07:43:31 -0300

    exercicio_3 - resolvido

--- exercicio_3.py
+++ exercicio_3.py
@@ -23,3 +23,10 @@ Os números ímpares são [1, 3, 5]
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
+lista_impar = []
+
+for i in range(len(lista)):
+    valor = lista[i]
+    if valor % 2 != 0:
+        lista_impar.append(valor)
+print(f'Resposta: {lista_impar}')
